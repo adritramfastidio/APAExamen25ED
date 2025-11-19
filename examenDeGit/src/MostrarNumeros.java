d8cfbcf2cbe14063f188070a2f2074154ab81783
adritramfastidio <adritramfastidio@gmail.com>
2025-11-19 11:53:23 +0100

    cambio calculadora sumar
    Sumar, multiplicar y elevar a una potencia

--- examenDeGit/src/MostrarNumeros.java
+++ examenDeGit/src/MostrarNumeros.java
@@ -12,4 +12,15 @@ public class MostrarNumeros {
         System.out.println("Número 2: " + numero2);
         sc.close();
     }
+
+    public static double sumar(double a, double b) {
+        return a + b;
+    }
+    public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public static double potencia(double base, double exponente) {
+        return Math.pow(base, exponente);
+    }
 }
